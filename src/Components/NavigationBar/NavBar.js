bb9bdbc4614809a56ca1da12385ccb344b25128b
Keannu Bernasol <noehbernasol0@gmail.com>
2022-10-04 11:51:51 +0800

    Added navigation bar

--- src/Components/NavigationBar/NavBar.js
+++ src/Components/NavigationBar/NavBar.js
@@ -1,13 +1,27 @@
 import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 export default function NavBar() {
     return (
-      <div>
-        <Link to="/home">
-          <button variant="outlined">
-            Home
-          </button>
-        </Link>
+      <div style={{backgroundColor: 'black'}}>
+        <br/>
+        <ButtonGroup>
+          <Link to="/home">
+            <Button>
+              Home
+            </Button>
+          </Link>
+          &nbsp;
+          <Link to="/about">
+            <Button>
+              About
+            </Button>
+          </Link>
+        </ButtonGroup>
+        <br/>
       </div>
     );
 }
--- src/Components/PageHandler/Main.js
+++ src/Components/PageHandler/Main.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Routes ,Route } from 'react-router-dom';
+
 import Homepage from '../../Pages/Homepage/Homepage';
+import About from '../../Pages/About/About';
 
 const Main = () => {
   return (
     <Routes> {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path='/Home' element={<Homepage/>}></Route>
+      <Route exact path='/About' element={<About/>}></Route>
     </Routes>
   );
 }
